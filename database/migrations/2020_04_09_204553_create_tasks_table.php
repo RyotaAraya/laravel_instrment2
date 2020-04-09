48057e9774499f72ad00e4a741934a1cb0991b4b
RARAYA <camerondiaz1215@gmail.com>
2020-04-09 21:35:47 +0900

    環境構築
    DB作成
    seeder作成

---
+++ database/migrations/2020_04_09_204553_create_tasks_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('plant_name');
+            $table->string('tag_no');
+            $table->string('trouble_content');
+            $table->string('details_repair');
+            $table->string('task_status');
+            $table->string('picture1');
+            $table->string('picture2');
+            $table->boolean('delete_flg');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
---
+++ database/seeds/TasksTableSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+use Carbon\Carbon;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class TasksTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        for ($i = 1; $i <= 20; $i++) {
+            DB::table('tasks')->insert([
+                'plant_name' => $i.'BH',
+                'tag_no' => 'FIC-'.$i,
+                'trouble_content' => '指示不審',
+                'details_repair' => 'ノズル貫通',
+                'task_status' => '完了',
+                'picture1' => 'picture1-'.$i,
+                'picture2' => 'picture2-'.$i,
+                'delete_flg' => 0,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ]);
+        }
+    }
+}
