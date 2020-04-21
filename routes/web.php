cb9b87264bdc1d8ef6e658882c917adcaa44fe7e
RyotaAraya <camerondiaz1215@gmail.com>
2020-04-21 21:10:08 +0900

    ルート作成

--- routes/web.php
+++ routes/web.php
@@ -29,4 +29,4 @@ Route::group(['middleware' => 'auth'], function () {
 });
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', 'HomeController@index')->name('home');
